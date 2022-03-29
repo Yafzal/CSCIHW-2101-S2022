75574f2ded69262a2c6b4fe655670814c2644791
Yafzal <afzaly@go.student.edu>
2022-03-29 15:57:50 -0400

    commit

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,5 +1,6 @@
 package HW2;
 
+import java.util.Scanner;
 
 public class MathGames {
     // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
@@ -45,9 +46,79 @@ public class MathGames {
     // you do not need this main if you want to make a tester class
      public static void main(String[] args){
         
+            Scanner input = new Scanner(System.in);
+     
+            System.out.println("Enter any number");
+
+            double a = input.nextDouble();
+     
+            System.out.println("Enter any other number");
+
+
+
+            double b = input.nextDouble();
+
+
+            double c1 = Math.pow(a,2) + Math.pow(b,2);
+     
+            double c2 = Math.sqrt(c1);
+     
+            // Use System.out.printf for this println statement.
+            // This is the other assignment
+
+
+            System.out.println("Square root of: " + a + "^2 " + " + " + b + "^2 " + "= " + c2);
+              
+              System.out.println("Enter your grade");
+
+
+              double grade = input.nextDouble();
+              double gradeCeil = Math.ceil(grade);
+              
+              if(grade >=95)
+                  System.out.println("A++");
+              else if(gradeCeil >= 90 && gradeCeil <= 95)
+                  System.out.println("A-");
+              else if(gradeCeil <= 89 && gradeCeil >= 85)
+                  System.out.println("B+");
+              else if(gradeCeil < 85 && gradeCeil >= 80)
+                  System.out.println("B-");
+              else if(gradeCeil < 79 && gradeCeil >= 75)
+                  System.out.println("C+");
+              else if(gradeCeil < 75 && gradeCeil >= 70)
+                  System.out.println("C-");
+              else if(gradeCeil <= 69 && gradeCeil >= 65)
+                  System.out.println("D+");
+              else if(gradeCeil < 65 && gradeCeil >= 61)
+                  System.out.println("D");
+              else
+                  System.out.println("F");
+
+
+
+
+                  // Final Question
+     
+            System.out.println("Enter total bill");
+              double totalBill = Math.ceil(input.nextDouble());
+              
+              System.out.println("Enter total number of people splitting the bill");
+              int numofPpl = input.nextInt();
+              
+              System.out.println("Enter the percentage to tip");
+              double percent2Tip = input.nextDouble();
+              double percentAge = percent2Tip / 100;
+     
+            
+     
+            
+            
+         }
+     
+     
     }
 
 
 
 
-}
+
