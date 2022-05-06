dddf63ddeb2dc33c7f676bbb5c3e3b1f6678da26
Yafzal <afzaly@go.student.edu>
2022-05-06 14:08:26 -0400

    commit

--- Project2/Medals.java
+++ Project2/Medals.java
@@ -8,7 +8,6 @@ public class Medals {
     Create a program that will print a table of medal winner counts with row totals
     I have provided the initial values needed, all you need to do is put the logic for printing the table
     The table should look like this:
-
          Country   Gold    Silver    Bronze  Total
          Canada       1       0       1       2
           China       1       1       0       2
@@ -17,20 +16,15 @@ public class Medals {
           Japan       0       1       1       2
          Russia       0       1       1       2
   United States       1       1       0       2
-
     After that create an ARRAYLIST that is the total of each medal awarded (IE 4 gold, 4 silver, 5 bronze)
-    print the total of each medal 
+    print the total of each medal
     Hints:
         int rows = counts.length; (get the rows)
-        int cols = counts[0].length; (gets the columns)   
-        counts[j][i] in a nested for loop would get you the totals of the column  
+        int cols = counts[0].length; (gets the columns)
+        counts[j][i] in a nested for loop would get you the totals of the column
         Gold    Silver    Bronze
         4       4         5
-
-
-
     FINAL TABLE SHOULD LOOK LIKE THIS:
-
         Country    Gold    Silver    Bronze  Total
          Canada       1       0       1       2
           China       1       1       0       2
@@ -48,25 +42,25 @@ public class Medals {
         final int MEDALS = 3;
 
         String[] countries = {
-            "Canada",
-            "China",
-            "Germany",
-            "Korea",
-            "Japan",
-            "Russia",
-            "United States"
+                "Canada",
+                "China",
+                "Germany",
+                "Korea",
+                "Japan",
+                "Russia",
+                "United States"
         };
 
-        int[][] counts = 
-        { 
-            { 1, 0, 1 },
-            { 1, 1, 0 }, 
-            { 0, 0, 1 }, 
-            { 1, 0, 1 }, 
-            { 0, 1, 1 }, 
-            { 0, 1, 1 },
-            { 1, 1, 0 }
-        };
+        int[][] counts =
+                {
+                        { 1, 0, 1 },
+                        { 1, 1, 0 },
+                        { 0, 0, 1 },
+                        { 1, 0, 1 },
+                        { 0, 1, 1 },
+                        { 0, 1, 1 },
+                        { 1, 1, 0 }
+                };
         System.out.println("        Country    Gold    Silver    Bronze   Total");
 
 
@@ -79,30 +73,53 @@ public class Medals {
         // These print statements are just for reference for the table above
         // if you want to change them you can
         // HINTS:
-        // You will need a nested for loop 
+        // You will need a nested for loop
         // (IE for i less than COUNTRIES, and for j < MEDALS)
         // Process the ith row
         // Print each row element and update the row total
-        // Display the row total and print a new line
+        // Display the row total and print a new line'
+        for(int i=0;i<COUNTRIES;i++)
+        {
+            int total =0;
+            System.out.printf("%15s", countries[i]);
+            for(int j=0;j<MEDALS;j++)
+            {
+                System.out.printf("%8d", counts[i][j]);
+                total += counts[i][j];
+            }
+            System.out.printf("%8d\n", total);
+        }
 
 
 
         // Display the total medals of each medal class
-        // First create an Integer ArrayList called medalCounts 
+        // First create an Integer ArrayList called medalCounts
         // your rows are counts.length
         // your columns are counts[0].length
 
-        // Make a for loop that will calculate the sum for each column 
+        // Make a for loop that will calculate the sum for each column
         // hint: make a temp variable for the sum and then at the end of the for loop add each sum of
         // each column to the ArrayList medalCounts
         // print the Array list and chart to look like this
         // Gold    Silver    Bronze
         // 4       4         5
-     
+
+        ArrayList<Integer> medalCounts = new ArrayList<>();
+        for(int i = 0;i< counts[0].length;i++)
+        {
+            int temp = 0;
+            for(int j = 0 ; j < counts.length ; j++)
+            {
+                temp += counts[j][i];
+            }
+            medalCounts.add(temp);
+        }
+        System.out.println("          Total    Gold    Silver    Bronze   X");
+        System.out.printf("%15s", "");
+        for(int i =0;i<medalCounts.size();i++)
+        {
+            System.out.printf("%8d", medalCounts.get(i));
+        }
     }
-    
+
 }
-
-
-
-
